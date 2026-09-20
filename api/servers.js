bc9395fd0b9898a1030b289300c09c2132feb3cf
ip93123 <ip93123@gmail.com>
2026-09-20 10:27:56 +0530

    Move Vidy to first server position so it appears in visible top 5 buttons; reduce cache to 1h

--- api/servers.js
+++ api/servers.js
@@ -33,14 +33,17 @@ export default async function handler(req, res) {
     const e = episode || 1;
     
     urls = [
+      `https://vidy.st/tv/${id}/${s}/${e}?color=E8C97A&autoplay=true&nextEpisode=true&episodeSelector=true&autoplayNextEpisode=true`,
       `https://peachify.top/embed/tv/${id}/${s}/${e}?autoNext=5`,
       `https://vidsrc.me/embed/tv?tmdb=${id}&season=${s}&episode=${e}&autoplay=1`,
       `https://player.videasy.net/tv/${id}/${s}/${e}?autoplay=1`,
       `https://embedmaster.link/tv/${id}/${s}/${e}?autoplay=1`,
-      `https://vidcore.net/tv/${id}/${s}/${e}?autoplay=1&server=orbit`,
     ];
-    labels = ['Server 1', 'Server 2', 'Server 3', 'Server 4', 'Server 5'];
+    labels = ['Vidy ✨', 'Server 1', 'Server 2', 'Server 3', 'Server 4'];
     
+    urls.push(`https://vidcore.net/tv/${id}/${s}/${e}?autoplay=1&server=orbit`);
+    labels.push('Server 5');
+
     if (imdbId) {
       urls.push(`https://primesrc.me/embed/tv?imdb=${imdbId}&season=${s}&episode=${e}`);
       labels.push('Server 6');
@@ -50,21 +53,23 @@ export default async function handler(req, res) {
       `https://vixsrc.to/tv/${id}/${s}/${e}`,
       `https://cinesrc.st/embed/tv/${id}?s=${s}&e=${e}`,
       `https://anyembed.xyz/embed/tmdb-tv-${id}-${s}-${e}?logo=false`,
-      `https://vidfast.pro/tv/${id}/${s}/${e}?autoplay=1`,
-      `https://vidy.st/tv/${id}/${s}/${e}?color=E8C97A&autoplay=true&nextEpisode=true&episodeSelector=true&autoplayNextEpisode=true`
+      `https://vidfast.pro/tv/${id}/${s}/${e}?autoplay=1`
     );
-    labels.push('Server 7', 'Server 8', 'Server 9', 'Server 10', 'Vidy ✨');
+    labels.push('Server 7', 'Server 8', 'Server 9', 'Server 10');
 
   } else {
     // Movie
     urls = [
+      `https://vidy.st/movie/${id}?color=E8C97A&autoplay=true`,
       `https://peachify.top/embed/movie/${id}`,
       `https://vidsrc.me/embed/movie?tmdb=${id}&autoplay=1`,
       `https://player.videasy.net/movie/${id}?autoplay=1`,
       `https://embedmaster.link/movie/${id}?autoplay=1`,
-      `https://vidcore.net/movie/${id}?autoplay=1&server=orbit`,
     ];
-    labels = ['Server 1', 'Server 2', 'Server 3', 'Server 4', 'Server 5'];
+    labels = ['Vidy ✨', 'Server 1', 'Server 2', 'Server 3', 'Server 4'];
+
+    urls.push(`https://vidcore.net/movie/${id}?autoplay=1&server=orbit`);
+    labels.push('Server 5');
 
     if (imdbId) {
       urls.push(`https://primesrc.me/embed/movie?imdb=${imdbId}`);
@@ -75,13 +80,12 @@ export default async function handler(req, res) {
       `https://vixsrc.to/movie/${id}`,
       `https://cinesrc.st/embed/movie/${id}`,
       `https://anyembed.xyz/embed/tmdb-movie-${id}?logo=false`,
-      `https://vidfast.pro/movie/${id}?autoplay=1`,
-      `https://vidy.st/movie/${id}?color=E8C97A&autoplay=true`
+      `https://vidfast.pro/movie/${id}?autoplay=1`
     );
-    labels.push('Server 7', 'Server 8', 'Server 9', 'Server 10', 'Vidy ✨');
+    labels.push('Server 7', 'Server 8', 'Server 9', 'Server 10');
   }
 
-  // Add cache headers for edge caching
-  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
+  // Short cache so new servers appear quickly
+  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=300');
   res.status(200).json({ urls, labels });
 }
